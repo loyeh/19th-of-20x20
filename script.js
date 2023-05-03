78071eb214b35449fe27e34589daae296d6d17c8
loyeh <asadi.loyeh@gmail.com>
2023-05-03 17:40:50 +0330

    first commit, add gitignore file

--- script.js
+++ script.js
@@ -1,6 +1,2 @@
-document.body.style.height=window.innerHeight;
-const head_part=document.querySelector(".head_part")
-
-function toggle_header(){
-  head_part.classList.toggle("hide")
-}
+document.body.style.height = window.innerHeight;
+const head_part = document.querySelector(".head_part");
